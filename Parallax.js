46186a59a582d4983bb09c950faa9e128581ae83
jack <hello@jackmakesthings.com>
2014-06-02 01:36:26 -0400

    testing out rotateZ in place of translateZ for performance

--- Parallax.js
+++ Parallax.js
@@ -44,7 +44,7 @@
     cutoff = 0;
 
     if ( adjustment <= cutoff ) {
-      el.style[transformProp] = "translateY(" + adjustment + "px) translateZ(0px)";
+      el.style[transformProp] = "translateY(" + adjustment + "px) rotateZ(360px)";
     }
   }
 
